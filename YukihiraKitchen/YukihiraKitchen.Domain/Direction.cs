bd45cbd4c5989d4189c598998d61ba4b974e42b9
Bao phung <b.giaphung@gmail.com>
2021-05-24 17:05:58 -0500

    adding domain objects

---
+++ YukihiraKitchen/YukihiraKitchen.Domain/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukihiraKitchen.Domain
+{
+    public class Direction
+    {
+        public string Id { get; set; }
+        public Recipe Recipe { get; set; }
+        public Guid RecipeId { get; set; }
+        public string CookingDirection { get; set; }
+        public int CookingStepNumber { get; set; }
+    }
+}
---
+++ YukihiraKitchen/YukihiraKitchen.Domain/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukihiraKitchen.Domain
+{
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        public string RecipeName { get; set; }
+        public string Description { get; set; }
+        public int CookingDuration { get; set; }
+        public int Temperature { get; set; }
+
+        public ICollection<Direction> Directions { get; set; } = new List<Direction>();
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+    }
+}
